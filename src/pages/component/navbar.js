0870595c252629f7682b49dd89edef70b146b38d
Marcelo Tomporoski Perez <159451183+Marcelo-IFMS@users.noreply.github.com>
2024-11-14 13:15:04 +0000

    API Routes function handler

--- src/pages/component/navbar.js
+++ src/pages/component/navbar.js
@@ -15,6 +15,7 @@ export default function Menu() {
             <Nav.Link href="/noticias">Notícias</Nav.Link>
             <Nav.Link href="/promessa">Promessa</Nav.Link>
             <Nav.Link href="/pages">Page</Nav.Link>
+            <Nav.Link href="/mesportes">P.Esportes</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
