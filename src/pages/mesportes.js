f68117a2c6989ed13944f21f4369fcd05bd4837a
Marcelo Tomporoski Perez <159451183+Marcelo-IFMS@users.noreply.github.com>
2024-11-29 13:05:37 +0000

    aula dia 29/11

--- src/pages/mesportes.js
+++ src/pages/mesportes.js
@@ -1,18 +1,42 @@
-import { Col, Container } from "react-bootstrap";
+import { Container, Table } from "react-bootstrap";
 import Menu from "./component/navbar";
 import Footer from "./component/footer";
 
 export default function mesportes() {
-    return <>
-        < Menu />
-        <Container>
-            <Container className="text-center rounded-pill border border-primary col-8">
-                Produtos de esportes
-            </Container>
-            <Container>
-                dados
-            </Container>
+  return (
+    <>
+      <Menu />
+      <Container>
+        <Container className="text-center rounded-pill border border-primary col-8">
+          Produtos de esportes
         </Container>
-        <Footer />
+        <Container>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Produto</th>
+                <th>Categoria</th>
+                <th>Descrição</th>
+                <th>Preço</th>
+              </tr>
+            </thead>
+            <tbody>
+{/* aqui será inserido o 
+laço de repetição para os produtos */}
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+{/* fim laço */}
+            </tbody>
+          </Table>
+        </Container>
+      </Container>
+      <Footer />
     </>
+  );
 }
